6ce5c236b97f5f6da589d55c1028eb798373d342
prabhasydv <142390148+prabhasydv@users.noreply.github.com>
2023-12-11 17:56:01 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,162 @@
+
+import './App.css';
+import { MdEmail } from "react-icons/md";
+import { IoCallSharp } from "react-icons/io5";
+
+
+
+
+
+
+function App() {
+  return (
+
+    
+
+    <>
+    <div className="screen">
+        <div className="screen2">
+
+        </div>
+
+        <div className="container">
+        <p className="text">Prabhas yadav </p> <p className="text2">
+          Front-End devloper :)</p>
+        </div>
+    
+    </div>
+    <h5 className="feel">HTML</h5>
+    <div className="skill">
+    <div className="outer">
+    <div className="inner">
+    <div className="number" id="number"> 95%</div></div>
+    </div>
+    
+
+
+
+    <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="160" height="160">
+    <defs>
+      <linearGradient id="GradientColor">
+        <stop offset="0%" stopColor="#e91e63" />
+        <stop offset="100%" stopColor="#673ab7" />
+      </linearGradient>
+    </defs>
+    <circle cx="80" cy="80" r="70" strokeLinecap="round" fill="url(#GradientColor)" />
+  </svg>
+      
+    </div>
+
+    <h5 id="heel">CSS</h5>
+    <div className="dkill">
+    <div className="outer">
+    <div className="inner">
+    <div className="number" id="number"> 98%</div></div>
+    </div>
+    
+
+
+
+    <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="160" height="160">
+    <defs>
+      <linearGradient id="GradientColor">
+        <stop offset="0%" stopColor="#e91e63" />
+        <stop offset="100%" stopColor="#673ab7" />
+      </linearGradient>
+    </defs>
+    <circle cx="80" cy="80" r="70" strokeLinecap="round" fill="url(#GradientColor)" className='cssmeter'/>
+  </svg>
+      
+    </div>
+
+    <h5 id="beel">Javascript</h5>
+    <div className="kill">
+    <div className="outer">
+    <div className="inner">
+    <div className="number" id="number"> 80%</div></div>
+    </div>
+    
+
+
+
+    <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="160" height="160">
+    <defs>
+      <linearGradient id="GradientColor">
+        <stop offset="0%" stopColor="#e91e63" />
+        <stop offset="100%" stopColor="#673ab7" />
+      </linearGradient>
+    </defs>
+    <circle cx="80" cy="80" r="70" strokeLinecap="round" fill="url(#GradientColor)" className='jsmeter'/>
+  </svg>
+      
+    </div>
+
+    <h5 id="weel">Wordpress</h5>
+    <div className="will">
+    <div className="outer">
+    <div className="inner">
+    <div className="number" id="number"> 70%</div></div>
+    </div>
+    
+
+
+
+    <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="160" height="160">
+    <defs>
+      <linearGradient id="GradientColor">
+        <stop offset="0%" stopColor="#e91e63" />
+        <stop offset="100%" stopColor="#673ab7" />
+      </linearGradient>
+    </defs>
+    <circle cx="80" cy="80" r="70" strokeLinecap="round" fill="url(#GradientColor)" className='wpmeter'/>
+  </svg>
+
+    <div className='resumefont'>
+      <h1>
+        Resume
+      </h1>
+    </div>
+      
+    </div>
+    <div className='resume'>
+
+    </div>
+    
+      <div className='fafa'>
+
+
+
+        
+      
+
+        
+        
+       </div>
+       <h1 className='logo'>         <MdEmail />
+</h1>
+      <a href='https://www.gmail.com'><h1 className='gmail'>
+        prabhasydv2000@gmail.com
+      </h1></a>
+      <h1 className='pogo'>         <IoCallSharp />
+
+</h1>
+      <a href='https://www.whatsapp.com'><h1 className='phone'>
+        +91 8368716225
+      </h1></a>
+
+    <h1 className='cont'>
+      Contact me 
+    </h1>
+
+       
+
+       
+
+    
+  </>
+    
+  );
+  
+}
+
+export default App;
